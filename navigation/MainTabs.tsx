d21bc54ed18b1647275bec75b05920ea4cc72e4a
Andrew <github@seventh.space>
2025-08-19 00:06:55 +0100

    Add tab icons and update What's Next tab label

--- navigation/MainTabs.tsx
+++ navigation/MainTabs.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
 import HomeScreen from "../screens/HomeScreen";
 import RecentActivityScreen from "../screens/RecentActivityScreen";
 import WhatsNextScreen from "../screens/WhatsNextScreen";
@@ -9,10 +10,43 @@ const Tab = createBottomTabNavigator();
 
 const MainTabs = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Recent" component={RecentActivityScreen} />
-    <Tab.Screen name="WhatsNext" component={WhatsNextScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Recent"
+      component={RecentActivityScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialIcons name="access-time" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="WhatsNext"
+      component={WhatsNextScreen}
+      options={{
+        tabBarLabel: "What's Next",
+        tabBarIcon: ({ color, size }) => (
+          <MaterialIcons name="lightbulb" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person" size={size} color={color} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
